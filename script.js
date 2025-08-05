4fb5c0718dca75e1006800654d618124b395b703
themarqueehouse <themarqueehouse@gmail.com>
2025-08-05 12:12:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Show loading screen immediately (keep it visible for 8 seconds)
+// Show loading screen immediately (keep it visible for 15 seconds)
 document.addEventListener("DOMContentLoaded", function () {
   // Start the timer right away
   setTimeout(function () {
     document.getElementById("loading-screen").style.display = "none";
-  }, 8000); // 8 seconds
+  }, 15000); // 15 seconds
 });
