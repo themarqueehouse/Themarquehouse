b3d60db250f732ea945cd60eac39ac1995c8dee9
themarqueehouse <themarqueehouse@gmail.com>
2025-08-04 23:25:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function () {
     setTimeout(() => {
         const loader = document.getElementById('loading-screen');
         loader.style.display = 'none';
-    }, 2500); // 4000 milliseconds = 4.0 seconds
+    }, 4000); // 4000 milliseconds = 4.0 seconds
 });
