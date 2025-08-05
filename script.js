dbbbfb4a5ded58d6d650eb71d1a804fecd761d77
themarqueehouse <themarqueehouse@gmail.com>
2025-08-04 22:29:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    setTimeout(() => {
+        const loader = document.getElementById('loading-screen');
+        loader.style.display = 'none';
+    }, 2000);
+});
